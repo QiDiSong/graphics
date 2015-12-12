c5bbfbb5135af0fbfa7f8f7607c3ed2d7d46b35a
Lucas Bongers <bongerlj@mcmaster.ca>
2015-12-12 00:19:18 -0500

    Added intersection tests

--- A3/scene_graph/plane.cpp
+++ A3/scene_graph/plane.cpp
@@ -16,7 +16,20 @@ Plane::~Plane()
 
 }
 
-bool Plane::intersects(Point p)
-{
-	return false;
+double Plane::intersects(double* near, double* far) {
+	topStuff = -(normal.x*origin.x + normal.y*origin.y + normal.z*origin.z + g);
+	bottomStuff = (origin.x*farPoint[0] + origin.y*farPoint[1] + origin.z*farPoint[2]);
+	t = topStuff/bottomStuff;
+	if(t <= 0 || bottomStuff = 0) {
+		result = false;
+		return result;
+	}
+	else {
+		result = true;
+		return result;
+	}
+	if(result) {
+		intersectionPoint = [nearPoint[0] + t*farPoint[0], nearPoint[1] + t*farPoint[1], nearPoint[2] + t*farPoint[2]];
+		return intersectionPoint;
+	}
 }
